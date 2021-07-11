62be3fd30698921bad45d17480501557c7937b3f
t.sano <t.sano@rlg.sys.es.osaka-u.ac.jp>
2021-07-11 13:54:57 +0900

    wip

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -x
-IMAGE_NAME="my-mape"
+IMAGE_NAME="ghcr.io/t-sano1222/multiagent-particle-envs/mape"
 IMAGE_TAG="v0.0.0" 
 cd ..
 docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f Docker/Dockerfile .
